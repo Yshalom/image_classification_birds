02569a7cd6b127be079ad5bb5746e25fe51e6643
Isaac-Desktop <yg2sh@DESKTOP0>
2026-08-24 00:01:22 +0200

    Update constant values

--- src/constants.py
+++ src/constants.py
@@ -1,4 +1,4 @@
-DB_PATH = "database/birds-525-species-image-classification/data/test-00000-of-00001.parquet"
+DB_TEST_PATH = "database/birds-525-species-image-classification/data/test-00000-of-00001.parquet"
 README_PATH = "database/birds-525-species-image-classification/README.md"
 
 # Path of the class label names in the README.md nested structure
@@ -21,3 +21,6 @@ LABEL_NAME_PATH = (
     "class_label:",
     "names:"
 )
+
+NUM_OF_CLASSES = 525
+IMAGE_SIZE = (224, 224)
